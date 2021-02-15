6d0837dc1082c0e32f798ac025548053f0108b45
Matt Perry <touche112@gmail.com>
2021-02-14 23:44:32 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,33 +4,31 @@
 echo "deb http://raspbian.raspberrypi.org/raspbian/ buster main contrib non-free rpi" | tee -a /etc/apt/sources.list
 apt update
 
-## install framebuffer, supervisor, novnc, and dwm
+## install virtual framebuffer, supervisor, novnc, and dwm
 apt install xvfb supervisor fluxbox novnc
 
 ## fix supervisor net issue
 unlink /var/run/supervisor.sock
 
 ## install old x11vnc version with tearing fix
-sudo apt-get install libvncserver0=0.9.9+dfsg2-6.1+deb8u8 x11vnc=0.9.13-1.2 x11vnc-data=0.9.13-1.2
+sudo apt install libvncserver0=0.9.9+dfsg2-6.1+deb8u8 x11vnc=0.9.13-1.2 x11vnc-data=0.9.13-1.2
 
 ##start install for build reqs
 apt install git qtbase5-dev qt5-default qt5-qmake libqt5serialport5-dev libusb-1.0
 
 ##download and build usbreset
-
+wget "https://raw.githubusercontent.com/TokugawaHeavyIndustries/DomePi/main/usbreset.c"
 cc usbreset.c -o usbreset
 chmod +x usbreset
 mv usbreset /etc/
+rm usbreset.c
 
 ##buld app
-mkdir /root/ddd
-cd /root/ddd
+mkdir /etc/ddd
+cd /etc/ddd
 git clone https://github.com/simoninns/DomesdayDuplicator
 cd ./DomesdayDuplicator/Linux-Application/DomesdayDuplicator
 qmake
 make all
 make install
 
-#remove build dir
-cd ~
-rm -r -f /root/ddd
