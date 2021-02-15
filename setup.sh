2251a6f3416a3f86b5209406c98749275480285b
Matt Perry <touche112@gmail.com>
2021-02-14 23:38:49 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 apt update
-apt install xvfb x11vnc supervisor fluxbox
+apt install xvfb supervisor fluxbox
 unlink /var/run/supervisor.sock
+
+sudo apt-get install libvncserver0=0.9.9+dfsg2-6.1+deb8u8 x11vnc=0.9.13-1.2 x11vnc-data=0.9.13-1.2
 
 ##start install for build reqs
 apt install git qtbase5-dev qt5-default qt5-qmake libqt5serialport5-dev libusb-1.0
