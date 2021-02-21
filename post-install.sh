ea7fafeefd505259323ff9ca1bdf09fc9b6b52b8
Matt Perry <touche112@gmail.com>
2021-02-21 17:28:48 -0500

    syntax

--- post-install.sh
+++ post-install.sh
@@ -522,7 +522,8 @@ if ($capturech -eq 1) {
 
     localcapconfig
             $cifsserverinstalled = apt-cache policy samba-common-bin | grep 'none'
-            if ($cifsserverinstalled -ne $NULL) {
+            $cifsserverinstalled2 = apt-cache policy samba | grep 'none'
+            if ($cifsserverinstalled -ne $NULL -or $cifsserverinstalled2 -ne $NULL) {
                 echo "samba-common samba-common/workgroup string  WORKGROUP" | sudo debconf-set-selections
                 echo "samba-common samba-common/dhcp boolean true" | sudo debconf-set-selections
                 echo "samba-common samba-common/do_debconf boolean true" | sudo debconf-set-selections
