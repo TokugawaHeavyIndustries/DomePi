e93cecdaa6b510a31490183530958a78e389cd9b
Matt Perry <touche112@gmail.com>
2021-08-17 23:28:03 -0400

    Update ledblink.sh

--- ledblink.sh
+++ ledblink.sh
@@ -4,7 +4,6 @@ while :
 do
 echo 1 | sudo tee /sys/class/leds/led0/brightness
 sleep .5
-echo 1 | sudo tee /sys/class/leds/led0/brightness
 echo 0 | sudo tee /sys/class/leds/led0/brightness
 sleep 1
 done
